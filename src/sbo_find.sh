724d885f1c69804936ba4fd4c9617d57f9bee808
Antonio Hernández Blas <hba.nihilismus@gmail.com>
2012-04-04 23:12:54 -0500

    New: sbo_find.sh to search for a SlackBuild.
    This script searchs through the local repository for a
    given SlackBuild name and prints all the results.

---
+++ src/sbo_find.sh
@@ -0,0 +1,73 @@
+#!/usr/bin/env bash
+
+me=$(basename $0)
+
+
+usage() {
+cat << EOF
+Usage:
+$me SlackBuild_name
+
+About:
+$me searchs for a SlackBuild directory in the local repository
+matching the given SlackBuild_name. It makes the search in case
+insensitive. You can use the wildcard '*' in SlackBuild_name,
+and if this is the case you must enclose it with single quotes
+to avoid pathname expansion by the shell.
+
+Examples:
+
+  This prints all SlackBuilds' directories inside the repository:
+    $me '*'
+
+  This prints all SlackBuilds' directories that starts with 'py'.
+    $me 'py*'
+
+  This prints all SlackBuilds' directories that ends with 'kernel'.
+    $me '*kernel'
+EOF
+}
+
+SBO_LOCAL_REPOSITORY=${SBO_LOCAL_REPOSITORY:-/usr/ports}
+
+set -f
+set -e
+
+if [ -z "$SLACKWARE_VERSION" ]; then
+    echo "$me: Error, enviroment variable SLACKWARE_VERSION is not defined"
+    exit 1
+fi
+
+SBO_LOCAL_REPOSITORY=$SBO_LOCAL_REPOSITORY/$SLACKWARE_VERSION
+
+if [ ! -d $SBO_LOCAL_REPOSITORY/ ]; then
+    echo "$me: Error, directory $SBO_LOCAL_REPOSITORY does not exist."
+    exit 1
+fi
+
+case $# in
+    0)
+        usage
+        exit 1
+        ;;
+    1)
+        if [ $1 = "--help" ]; then
+            usage
+            exit
+        fi
+        search=$1
+        ;;
+    *)
+        usage
+        exit 1
+        ;;
+esac
+
+results=$(find $SBO_LOCAL_REPOSITORY/ -maxdepth 2 -mindepth 2 -type d -iname "$search")
+
+if [ -z "$results" ]; then
+    echo "$me: Error, no results for '$search'"
+    exit 1
+fi
+
+echo $results | sed 's/ /\n/g'
