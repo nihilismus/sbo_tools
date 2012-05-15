c17a062cd6eeb0959f03032a023610b92828cc83
Antonio Hernández Blas <hba.nihilismus@gmail.com>
2012-05-15 17:45:08 -0500

    Change: delete use of enviroment variables

--- src/sbo_sync.sh
+++ src/sbo_sync.sh
@@ -11,16 +11,9 @@ About:
 $me synchronize a local repository of Slackware build scripts
 from http://www.slackbuilds.org project.
 
-The local repository is defined by the enviroment variable
-SBO_LOCAL_REPOSITORY, by default it is set to /usr/ports.
-
-Based in the enviroment variable SLACKWARE_VERSION, the remote
-repository would be syncronized in SBO_LOCAL_REPOSITORY, since
-there is not default value for this variable so must be defined.
+The local repository is set to /usr/ports.
 EOF
 }
-
-SBO_LOCAL_REPOSITORY=${SBO_LOCAL_REPOSITORY:-/usr/ports}
 
 set -e
 
@@ -34,35 +27,31 @@ if [ $(id --user) -ne 0 ]; then
     exit 1
 fi
 
-if [ -z "$SLACKWARE_VERSION" ]; then
-    echo "$me: Error, enviroment variable SLACKWARE_VERSION is not defined"
+slk_version=13.37
+local_repository="/usr/ports/$slk_version"
+
+if [ ! -d $local_repository/ ]; then
+    echo "$me: Error, directory $local_repository does not exist."
     exit 1
 fi
 
-SBO_LOCAL_REPOSITORY=$SBO_LOCAL_REPOSITORY/$SLACKWARE_VERSION
-
-if [ ! -d $SBO_LOCAL_REPOSITORY/ ]; then
-    echo "$me: Error, directory $SBO_LOCAL_REPOSITORY does not exist."
+if [ ! -f $local_repository/ChangeLog.txt ]; then
+    echo "$me: Error, file $local_repository/ChangeLog.txt does not exist."
     exit 1
 fi
 
-if [ ! -f $SBO_LOCAL_REPOSITORY/ChangeLog.txt ]; then
-    echo "$me: Error, file $SBO_LOCAL_REPOSITORY/ChangeLog.txt does not exist."
-    exit 1
-fi
-
-touch $SBO_LOCAL_REPOSITORY/.writable 2>&1
+touch $local_repository/.writable 2>&1
 if [ $? -ne 0 ]; then
-    echo "$me: Error, you do not have write permission in $SBO_LOCAL_REPOSITORY"
+    echo "$me: Error, $local_repository is not writable"
     exit 1
 fi
-rm -f $SBO_LOCAL_REPOSITORY/.writable
+rm -f $local_repository/.writable
 
 sbo_rsync_server="slackbuilds.org"
-sbo_rsync_module="slackbuilds/$SLACKWARE_VERSION"
+sbo_rsync_module="slackbuilds/$slk_version"
 
 # As a "security" step before executing rsync.
-cd $SBO_LOCAL_REPOSITORY/ || exit 1
+cd $local_repository/ || exit 1
 
 echo "$me: rsync:://$sbo_rsync_server/$sbo_rsync_module => $(pwd)"
 sleep 5
@@ -83,8 +72,8 @@ rsync \
     "$sbo_rsync_server"::"$sbo_rsync_module/" .
 
 if [ $? -ne 0 ]; then
-    echo "$0: Error, there was an error with rsync execution"
+    echo "$me: Error, there was an error with rsync execution"
     exit 1
 fi
 
-echo "$0: done"
+echo "$me: done"
