3cfa944f11b24e47e3505ef461470c92c88b8d48
Antonio Hernández Blas <hba.nihilismus@gmail.com>
2013-11-28 21:20:31 -0600

    Change: sbo_inst nows can receive an SlackBuild directory as parameter

--- src/sbo_inst.sh
+++ src/sbo_inst.sh
@@ -8,13 +8,18 @@ set -e
 usage() {
 cat << EOF
 Usage:
-$me [-n] [string]
+$me [[-n] string|directory]
 
 About:
 $me searches (incase sensitive) for a package name matching the
 given string inside /var/log/packages and print a formated output
 with the last status change time for each file which would (in most
 cases) indicate the date that package was installed.
+
+In case of a directory then it gets information from directory.info
+and directory.SlackBuild to see if the SlackBuild is installed
+(same VERSION and BUILD), if this is the case then prints a
+formated output just as the previous paragraph.
 
 Options:
 -n: print just the matching names.
@@ -26,6 +31,9 @@ Examples:
 
   This prints just the names of all packages that end with '_SBo'.
     $me -n '*_SBo'
+
+  If vlc is installed then prints its entry in /var/log/packages
+    $me \$(sbo_find -e vlc)
 
 EOF
 }
@@ -48,12 +56,32 @@ case $# in
             exit
         fi
 
-        if [ $1 = "-n" ]; then
+        if [ "$1" = "-n" ]; then
             search '*' | awk '{print $3}'
             exit
         fi
 
-        asterisk=$(echo $1 | grep '*' || echo '')
+        if [ -d "$1" ]; then
+            if $(sbo_info "$1" >/dev/null 2>&1); then
+                package=$(sbo_info "$1" | grep 'Package:' | sed 's/^.*: *//')
+                version=$(sbo_info "$1" | grep 'Version:' | sed 's/^.*: *//')
+                build=$(sbo_info "$1" | grep 'Build:' | sed 's/^.*: *//')
+                result=$(search "$package-$version-*-$build*" | awk '{print $3"\\n"}')
+
+                if [ -z "$result" ]; then
+                    echo "$me: $(basename $1) is not installed."
+                    exit 1
+                fi
+
+                echo -e $result | sed -e 's/^ //' -e '/^$/d'
+                exit 0
+            else
+                sbo_info "$1" | sed 's/sbo_info/sbo_inst/g'
+                exit 1
+            fi
+        fi
+
+        asterisk=$(echo "$1" | grep '*' || echo '')
         if [ -z "$asterisk" ]; then
             result=$(search "*$1*" | awk '{print $1" "$2" "$3"\\n"}')
         else
@@ -69,8 +97,8 @@ case $# in
         exit
         ;;
     2)
-        if [ $1 = "-n" ]; then
-            asterisk=$(echo $2 | grep '*' || echo '')
+        if [ "$1" = "-n" ]; then
+            asterisk=$(echo "$2" | grep '*' || echo '')
             if [ -z "$asterisk" ]; then
                 result=$(search "*$2*" | awk '{print $3"\\n"}')
             else
